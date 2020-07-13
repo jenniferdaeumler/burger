51ab1d7acd0a1e53fe74c75a0e8abf79fdaef9e6
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-13 07:42:04 -0500

    Config folder setup, connection js file created, connected node to mySQL and exported

--- server.js
+++ server.js
@@ -13,22 +13,6 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Gracie19!",
-    database: "moviePlannerDb"
-  });
-
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-  
-    console.log("connected as id " + connection.threadId);
-  });
 
   // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
