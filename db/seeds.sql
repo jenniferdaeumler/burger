580939086c6966a465e2b6cb46144bdc6091e795
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-13 07:39:01 -0500

    seeds file created. Three initial entries

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+--  Still in the `db` folder, create a `seeds.sql` file. In this file, write insert queries to populate the `burgers` table with at least three entries.
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("flamethrower", false), ("cowboy", false), ("sunrise", false);
+
