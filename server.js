ef995700ef81fcfca1a71a35c6a3b66e5c4df5f0
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-13 07:31:17 -0500

    App setup according to ReadMe

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const app = express();
+
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Gracie19!",
+    database: "moviePlannerDb"
+  });
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+  
+    console.log("connected as id " + connection.threadId);
+  });
+
+  // Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
