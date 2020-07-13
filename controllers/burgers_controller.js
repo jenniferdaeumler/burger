62d843a468a711d0bd92cd433235742e979e6890
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-13 08:26:27 -0500

    Router added to burger_controller.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const burgerjs = require("./models/burger.js")
+const router = express.Router();
 
 // Create the `router` for the app, and export the `router` at the end of your file.
+
+module.exports = router;
