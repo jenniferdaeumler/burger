944a45d89383d418bc0270872e7daff9fbdd1dc6
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-13 15:29:16 -0500

    Routers added get, put, and update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const burger = require("../models/burger.js")
 
 
-//router.get router.post 
+//Routers
+//Get route to display all burgers
 router.get("/", function(req, res){
     burger.all(function(data){
         const hbsObject = {
@@ -14,5 +15,36 @@ router.get("/", function(req, res){
         res.render("index", hbsObject);
     });
 });
+
+//Post route to display new burger submitted via form
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+
+//Put route to display updated "devoured" burgers
+router.put("/api/burgers/:id", function(req, res) {
+    const condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+
 
 module.exports = router;
