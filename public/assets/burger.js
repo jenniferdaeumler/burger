cd3ba02c5491748a3d7c62e34e854048a190c39a
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-13 19:25:23 -0500

    Fixed all the server errors. Changed queries etc

--- public/assets/burger.js
+++ public/assets/burger.js
@@ -1,19 +1,14 @@
 //Devoured button, PUT route
 $(function () {
-  $("devoured").on("click", function (event) {
-    const id = $(this).data("id");
-    const newDev = $(this).data("newDev");
+  $(".devoured").on("click", function (event) {
+    const id = $(this).data("burgerid");
 
-    var newDevState = {
-      devoured: newDev,
-    };
 
     // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newDevState,
+      type: "PUT"
     }).then(function () {
-      console.log("Changed devored to:", newDev);
+      // console.log("Changed devored to:", newDev);
       // Reload the page to get the updated list
       location.reload();
     });
@@ -25,7 +20,7 @@ $(function () {
     event.preventDefault();
 
     const newBurger = {
-      name: $("#bn").val().trim(),
+      burger_name: $("#bn").val().trim(),
     };
 
     // Send the POST request.
